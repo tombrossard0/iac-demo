b8be6db16d5e07401b2ea4c5d99c7a040478b6af
Tom Brossard <tombrossard0@gmail.com>
2025-09-15 11:51:25 +0200

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
   skip_metadata_api_check     = true
   skip_credentials_validation = true
   endpoints {
-    ec2 = "http://ip10-0-13-6-d33s7d9ntdlhbpdsdbfg-8080.direct.lab-boris.fr"
+    ec2 = "http://ip10-0-13-4-d33s7d9ntdlhbpdsdbfg-4566.direct.lab-boris.fr"  
   }
 }
 
